21f2d7cd780e3ea915e49b1f9c3ca4fcaa34136a
manibhushan <manibhushan.cs@gmail.com>
2015-05-05 19:56:57 +0530

    poker deal logic for a deck of cards

---
+++ lesson1/deal.py
@@ -0,0 +1,31 @@
+import random
+import random
+
+
+def deal(numhands, n=5, deck=[r+s for r in "23456789TJQKA" for s in "SHDC"]):
+    """ shuffle the deck and deal out numhands n-cards """
+    random.shuffle(deck)
+    return [deck[n*i:n*(i+1)] for i in range(numhands)]
+
+
+def deal_cards(numhands, n=5, deck):
+    random.shuffle(deck)
+    start = 0
+    end = n
+    result = []
+    while (numhands > 0) and (end < len(deck)):
+        result.append(deck[start:end])
+        start += n
+        end += n
+        numhands -= 1
+    # print result
+    return result
+
+
+def main():
+    mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC']
+    hands = deal_cards(7, 5, mydeck)
+    print hands
+
+if __name__ == '__main__':
+    main()
