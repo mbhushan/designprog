9f1956cff0dc402999ac41cc2efe0a139dfdbe23
manibhushan <manibhushan.cs@gmail.com>
2015-05-09 05:22:05 +0530

    added methods for experimental runs

--- utils/perf.py
+++ utils/perf.py
@@ -9,6 +9,16 @@ def timecall(fn, *args):
     return t1 - t0, result
 
 
+def timecalls(n, fn, *args):
+    """ Call fn n times with args; return min, avg, and max """
+    times = [timecall(fn, *args)[0] for _ in range(n)]
+    return min(times), average(times), max(times)
+
+
+def average(numbers):
+    return sum(numbers)/float(len(numbers))
+
+
 def func(n):
     if n == 0 or n == 1:
         return 1
@@ -20,6 +30,9 @@ def main():
     time, result = timecall(func, n)
     print "factorial: ", result
     print "time taken: ", time
+    x = input("how many experimental runs? ")
+    mn, avg, mx = timecalls(x, func, n)
+    print "min: %s, max: %s, avg: %s" % (mn, mx, avg)
 
 
 if __name__ == '__main__':
