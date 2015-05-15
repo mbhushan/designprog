252855346eb1b199dae69a1067b9b2434af8a7eb
manibhushan <manibhushan.cs@gmail.com>
2015-05-15 08:10:40 +0530

    trace template

---
+++ lesson3/trace.py
@@ -0,0 +1,17 @@
+
+
+@decorator
+def trace(f):
+    indent = '    '
+    def _f(*args):
+        signature = '%s(%s)' % (f.__name__, ', '.join(map(repr, args)))
+        print '%s--> %s' % (trace.level * indent, signature)
+        trace.level += 1
+        try:
+            print "%s<---%s === %s" % ((trace.level-1)*indent, signature,
+                                       result)
+        finally:
+            pass
+        return
+    trace.level = 0
+    return _f
