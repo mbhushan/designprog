e2472c8853e692184aac730fafbed52b692aa41c
manibhushan <manibhushan.cs@gmail.com>
2015-05-20 22:40:31 +0530

    circus alignment

--- musings/circus.py
+++ musings/circus.py
@@ -1,6 +1,6 @@
 """
 A circus is designing a tower routine consisting of people standing atop one
-another’s shoulders For practical and aesthetic reasons, each person must be
+anothers shoulders For practical and aesthetic reasons, each person must be
 both shorter and lighter than the person below him or her Given the heights and
 weights of each person in the circus, write a method to compute the largest
 possible number of peo- ple in such a tower
@@ -9,3 +9,16 @@ EXAMPLE:
     Output: The longest tower is length 6 and includes from top to bottom:
         (56, 90) (60,95) (65,100) (68,110) (70,150) (75,190)
 """
+
+
+def circus_design(people_list):
+    print sorted(people_list, key=lambda x: x[0])
+
+
+def main():
+    people_list = [(65, 92), (70, 150), (56, 90),
+                   (75, 190), (60, 95), (68, 110)]
+    circus_design(people_list)
+
+if __name__ == '__main__':
+    main()
